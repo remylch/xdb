350bf12d26832d0a1d5919a315513cb573a52e31
Zulgsmh <44394308+Zulgsmh@users.noreply.github.com>
2024-08-20 00:06:57 +0200

    fix: docker compose file to test separated nodes

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	env "github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
-	"time"
+	"strings"
 	"xdb/p2p"
 )
+
+func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "healthy"})
+}
+
+func startHttpServer() {
+	http.HandleFunc("/health", healthcheckHandler)
+	_ = http.ListenAndServe(os.Getenv("HTTP_ADDR"), nil)
+}
 
 func main() {
 	err := env.Load()
@@ -18,32 +29,40 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 
-	s1 := p2p.MakeServer("./data/s1ddir", ":3000")
-	s2 := p2p.MakeServer("./data/s2ddir", ":4000", ":3000")
-	s3 := p2p.MakeServer("./data/s3ddir", ":6000", ":4000", ":3000")
+	peers := strings.Split(os.Getenv("BOOTSTRAP_NODES"), ",")
+	s1 := p2p.MakeServer("./data/s1ddir", os.Getenv("NODE_ADDR"), peers...)
 
-	servers := []*p2p.Server{s1, s2, s3}
+	go startHttpServer()
 
-	for _, s := range servers {
-		go func(s *p2p.Server) {
-			if err := s.Start(); err != nil {
-				log.Fatalln(err)
-			}
-		}(s)
-	}
-
-	time.Sleep(2 * time.Second)
-
-	p2p.SendTestMessage(s3, "test")
-
-	time.Sleep(2 * time.Second)
-
-	b1 := s1.Retrieve("test")
-	b2 := s2.Retrieve("test")
-	b3 := s3.Retrieve("test")
-
-	log.Println(bytes.Equal(b1, b2))
-	log.Println(bytes.Equal(b2, b3))
+	s1.Start()
 
 	select {}
+
+	//s2 := p2p.MakeServer("./data/s2ddir", ":4000", ":3000")
+	//s3 := p2p.MakeServer("./data/s3ddir", ":6000", ":4000", ":3000")
+	//
+	//servers := []*p2p.Server{s1, s2, s3}
+	//
+	//for _, s := range servers {
+	//	go func(s *p2p.Server) {
+	//		if err := s.Start(); err != nil {
+	//			log.Fatalln(err)
+	//		}
+	//	}(s)
+	//}
+	//
+	//time.Sleep(2 * time.Second)
+	//
+	//p2p.SendTestMessage(s3, "test")
+	//
+	//time.Sleep(2 * time.Second)
+	//
+	//b1 := s1.Retrieve("test")
+	//b2 := s2.Retrieve("test")
+	//b3 := s3.Retrieve("test")
+	//
+	//log.Println(bytes.Equal(b1, b2))
+	//log.Println(bytes.Equal(b2, b3))
+	//
+	//select {}
 }

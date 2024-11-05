8e5c8f56b5ea4bd6568c293e40ce13048912769a
remylch <44394308+remylch@users.noreply.github.com>
2024-11-05 22:33:00 +0100

    feat(ui): display collections (#2)
    Co-authored-by: Rémy Loche <remy.loche-external@geopost.com>

---
+++ ui/src/pages/collection-page.tsx
@@ -0,0 +1,16 @@
+import {useParams} from "react-router-dom";
+import {useCollectionData} from "../hook/useCollectionData.ts";
+import {Loader} from "../component/loader.tsx";
+
+export const CollectionPage = () => {
+    const {collection} = useParams()
+
+    const {error, isLoading, data} = useCollectionData(collection!)
+
+    return <div className="flex flex-col gap-5">
+            <span>{collection}</span>
+            {error ? <span>Unable to load collection data</span> : null}
+            {isLoading && <Loader text={`Loading ${collection} collection data`} />}
+            {data && <div>{data.data}</div>}
+    </div>
+}
